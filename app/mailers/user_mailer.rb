25168d6bdddbb1037d288b3bac5a6ee8672eca80
Linchpin <you@example.com>
2016-08-04 15:16:22 +0530

    Added email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,6 @@ class UserMailer < ApplicationMailer
 	def welcome_email(user)
     @user = user
     @url  = 'http://example.com/login'
-    email_body = ''
     mail(to: @user.email,
          subject: "Already rendered!")
   	end
